49c6ab446e903ff6408bc956d28c05e934e43415
Arbit3r <rikukoski2@gmail.com>
2022-10-02 18:16:28 +0300

    Kirjanhaku rajapinta

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -8,17 +8,15 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("yeet");
-<<<<<<< HEAD
+
 		Rajapinta n = new Rajapinta();
 		n.yhteys("9781974725915");
-=======
 		System.out.println("jeeep");
 		System.out.println("jeeep");
 		System.out.println("jeeep");
 		System.out.println("12345678");
 		System.out.println("12345678");
 		System.out.println("12345678");
->>>>>>> 5408dfeba027138a5d3dde7bd11d53a671940b08
 	}
 
 }
